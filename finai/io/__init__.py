79b2e5711b387d5da4db34f20bbf16a2d494d181
jake-lehrman <141569591+jake-lehrman@users.noreply.github.com>
2025-08-08 09:47:12 -0700

    Create __init__.py

---
+++ finai/io/__init__.py
@@ -0,0 +1,9 @@
+"""
+I/O utilities and initializations for the finai toolkit.
+
+This module initializes the io package, which contains helper functions and
+classes for reading and writing datasets, managing data caches, and
+handling file system interactions.
+"""
+
+__all__ = []
