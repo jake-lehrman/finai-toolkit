93fb0555307f2592f431bd5e14afb612ed03a898
jake-lehrman <141569591+jake-lehrman@users.noreply.github.com>
2025-08-08 09:18:07 -0700

    Create __init__.py

---
+++ finai/nlp/__init__.py
@@ -0,0 +1,8 @@
+"""
+NLP module for event extraction, sentiment analysis, and summarization.
+
+This module contains functions and classes that interface with financial-specific NLP models,
+including sentiment models, event detectors, and summarizers.
+"""
+
+__all__ = []
